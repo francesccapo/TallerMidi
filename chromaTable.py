64c28eee54ceead078a76f11db45a241d2167341
francesccapo <francesc.capo@upf.edu>
2015-03-04 13:46:08 +0100

    ----- ChromaTable -------- Completed feaures: midi load, duration midi, midi conversor & cut midi

--- chromaTable.py
+++ chromaTable.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import pdb
 
 
 smf2txt = './smf2txt'
@@ -8,37 +9,112 @@ smf2txt = './smf2txt'
 
 class Track():
     def __init__(self):
+        self.trackID = []
         self.notesNum = []
         self.name = []
         self.info = np.empty([0, 4])
 
-    
-def chromaTable(midifile):
+
+def midiload(midifile):
     txttemp = 'temp.txt'
     order = smf2txt + ' -q 1024 ' + midifile + ' > ' + txttemp
-    try: os.system(order)
+    try:
+        os.system(order)
     except:
         print 'ERROR on ' + order
         sys.exit(1)
-        
-    f = open(txttemp,'r')
+
+    f = open(txttemp, 'r')
     midi_lines = f.readlines()
     f.close()
-    
-    
+
     tracklist = []
     cont = -1
+    notes = 0
     for midi_line in range(len(midi_lines)):
         midi_lines[midi_line] = midi_lines[midi_line][:-1]
-        if midi_lines[midi_line].find('#') != -1 or midi_lines[midi_line].find('@') != -1 :
-            if midi_lines[midi_line].find('track') != -1: ##pensar a posar excepcions
+        if midi_lines[midi_line].find('#') != -1 or midi_lines[midi_line].find('@') != -1:
+            if midi_lines[midi_line].find('track') != -1:
                 cont += 1
                 tracklist.append(Track())
                 tp = midi_lines[midi_line].split(' ')
-                tracklist[cont].name = tp[2]
-                notes = 0
+                tracklist[cont].trackID = tp[1]
+                tracklist[cont].name = ' '.join(tp[2:])
+                if notes != 0:
+                    tracklist[cont - 1].notesNum = notes
+                    notes = 0
         else:
-            tracklist[cont].info = np.vstack([tracklist[cont].info, np.array(midi_lines[midi_line].split(' '))])
+            tp = np.array(midi_lines[midi_line].split(' '))
+            tracklist[cont].info = np.vstack([tracklist[cont].info, tp.astype(np.float)])
             notes += 1
 
+    tracklist[cont].notesNum = notes
+
+    rmlist = []
+
+    for i in range(len(tracklist)):
+        if tracklist[i].notesNum == [] or tracklist[i].notesNum == 0:
+            rmlist.append(i)
+    for i in range(len(rmlist) - 1, -1, -1):
+        tracklist.__delitem__(rmlist[i])
+    print 'Midi "' + midifile + '" loaded'
     return tracklist
+
+
+def durationmidi(tracklist):
+    maxtime = 0
+    for i in range(len(tracklist)):
+        if tracklist[i].notesNum >= 20:
+            for j in range(tracklist[i].notesNum - 1, tracklist[i].notesNum - 21, -1):
+                if maxtime < (tracklist[i].info[j][1] + tracklist[i].info[j][2]):
+                    maxtime = tracklist[i].info[j][1] + tracklist[i].info[j][2]
+        else:
+            for j in range(tracklist[i].notesNum - 1, -1, -1):
+                if maxtime < (tracklist[i].info[j][1] + tracklist[i].info[j][2]):
+                    maxtime = tracklist[i].info[j][1] + tracklist[i].info[j][2]
+    print 'Midi duration computed'
+    maxtime = int(maxtime / 1024)
+    return maxtime
+
+
+def cutmidi(tracklist, maxbeat):
+    for i in range(len(tracklist)):
+        for j in range(tracklist[i].notesNum - 1):
+            if tracklist[i].info[j][1] > maxbeat * 1024:
+                tracklist[i].info = tracklist[i].info[:j]
+                tracklist[i].notesNum = j
+                break
+
+    print 'Midi cut'
+    return tracklist
+
+
+def midiconversor(line):
+    (integ, note) = divmod(line[0], 12)  # Note: (integer, residual)
+    (pos, res) = divmod(line[1], 1024)
+    duration = line[2]
+    current = line[1]
+    matrix = []
+    while duration > 0.0:
+        tmp = np.zeros(12)
+        (pos_2, res_2) = divmod(current, 1024)
+        if duration < 1024 - res_2:
+            tmp[note] = duration
+        else:
+            tmp[note] = 1024 - res_2
+        current += tmp[note]
+        duration -= tmp[note]
+        matrix.append(tmp/1024)
+    return int(pos), matrix
+
+
+def chromatablecreation(tracklist):
+    table = np.zeros((12,durationmidi(tracklist)))
+    for i in range(len(tracklist)):
+        for note in range(tracklist[i].notesNum):
+            (pos, mat) = midiconversor(tracklist[i].info[note])
+            for col in range(len(mat)):
+                table[:, pos] = table[:, pos] + mat[col]
+    print 'Chroma table created'
+    return table
+
